a13b7463bbf6ad3d0ed46a0cc08d4d44af83e24d
Wolves <cstingl@protonmail.com>
2025-01-31 09:58:44 -0500

    update and fix config issues

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -104,7 +104,7 @@ in {
   };
 
   fonts.packages = [
-    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    pkgs.nerd-fonts.jetbrains-mono
   ];
 
   system.activationScripts.applications.text = let
@@ -120,7 +120,7 @@ in {
       rm -rf /Applications/Nix\ Apps
       mkdir -p /Applications/Nix\ Apps
       find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-      while read src; do
+      while read -r src; do
         app_name=$(basename "$src")
         echo "copying $src" >&2
         ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
